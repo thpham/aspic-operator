29bba370d967c0e88b5224bf8fe72a401b93e05c
Thomas Pham <thomas.pham@ithings.ch>
2022-04-21 18:06:37 +0200

    fix helm dependencies update and CI

--- shell.nix
+++ shell.nix
@@ -1,5 +1,18 @@
 # shell.nix
+
+## To PIN dependencies in time
+#with (import (builtins.fetchGit {
+  # Descriptive name to make the store path easier to identify
+#  name = "nixos-unstable-2022-04-26";
+#  url = "https://github.com/nixos/nixpkgs/";
+  # Commit hash for nixos-unstable as of 2022-04-26
+  # `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
+#  ref = "refs/heads/nixos-unstable";
+#  rev = "6a323903ad07de6680169bb0423c5cea9db41d82";
+#}) {});
+
 with (import <nixpkgs> {});
+
 let
 
   python-with-my-packages = python38.withPackages (ps: [
